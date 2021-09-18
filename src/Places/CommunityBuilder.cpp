db907f38b761262907f20e6fd4c440663f7c4abc
Etspüler Manuel <29165243+Neithari@users.noreply.github.com>
2021-09-19 01:12:26 +0200

    Bugfix and implementation of workplace assignement
    Bugfix when adding workplaces
    Fixed the distribution numbers to be a sum of 100%, was 99.9%.
    Implemented the weighted assigning of workplaces

--- src/Places/CommunityBuilder.cpp
+++ src/Places/CommunityBuilder.cpp
@@ -39,13 +39,13 @@ void DeseaseSpreadSimulation::CommunityBuilder::CreatePlaces(size_t populationSi
 		}
 	}
 	// Workplace size estimates https://www.statista.com/statistics/944669/current-office-size-full-time-employees-usa/
-	std::array<float, 5> workplaceSize{ 0.2648f, 0.308f, 0.1908f, 0.0821f, 0.1542f };
+	std::array<float, 5> workplaceSize{ 0.2649f, 0.308f, 0.1908f, 0.0821f, 0.1542f };
 	float workplaceCount = 0.f;
 
 	// Then get the workplace counts for the size groups and sum them
 	for (size_t i = 0; i < workplaceSize.size(); i++)
 	{
-		workplaceCount = (workingPeople * workplaceSize.at(i)) / (25 + 50 * i);
+		workplaceCount += (workingPeople * workplaceSize.at(i)) / (25 + 50 * i);
 	}
 	
 	// Add the calculated amount of workplaces to the community
@@ -73,6 +73,49 @@ void DeseaseSpreadSimulation::CommunityBuilder::CreatePopulation(size_t populati
 {
 	PersonPopulator personCreator(populationSize, PersonPopulator::GetCountryDistribution(country));
 
+	// Create a vector with all workplaces to assigne them later
+	std::vector<Place*> workplaces;
+
+	for (const auto& place : outCommunity.GetPlaces())
+	{
+		// TODO: implement sorting for workplace and assign a workplace to the person
+		if (place->GetType() == Place::Place_Type::Workplace)
+		{
+			workplaces.push_back(place.get());
+		}
+	}
+
+	// First get number of working people
+	auto countryDistribution = std::move(PersonPopulator::GetCountryDistribution(country));
+	size_t workingPeople = 0;
+	for (const auto& distribution : countryDistribution)
+	{
+		if (distribution.ageGroup > Age_Group::UnderTwenty && distribution.ageGroup <= Age_Group::UnderSeventy)
+		{
+			workingPeople += static_cast<size_t>(populationSize / distribution.percent);
+		}
+	}
+
+	// Then get the workplace counts for the size groups and transfer the right amount into a vector inside the bySize array
+	// Workplace size estimates https://www.statista.com/statistics/944669/current-office-size-full-time-employees-usa/
+	std::array<float, 5> workplaceSize{ 0.2649f, 0.308f, 0.1908f, 0.0821f, 0.1542f };
+	std::array<std::vector<Place*>, 5> workplacesBySize;
+	for (size_t i = 0; i < workplaceSize.size(); i++)
+	{
+		for (size_t j = 0; j < static_cast<size_t>((workingPeople * workplaceSize.at(i)) / (25 + 50 * i)); j++)
+		{
+			workplacesBySize.at(i).push_back(workplaces.back());
+			workplaces.pop_back();
+		}
+	}
+	// Assigne leftovers to under 20 people workplaces
+	while (workplaces.size() > 0)
+	{
+		workplacesBySize.at(0).push_back(workplaces.back());
+		workplaces.pop_back();
+	}
+
+	// Create the population
 	while (true)
 	{
 		auto person = personCreator.GetNewPerson(AssignHome());
@@ -86,13 +129,15 @@ void DeseaseSpreadSimulation::CommunityBuilder::CreatePopulation(size_t populati
 		// Assigne a workplace when the person is in working age
 		if (person->GetAgeGroup() > Age_Group::UnderTwenty && person->GetAgeGroup() <= Age_Group::UnderSeventy)
 		{
-			for (const auto& place : outCommunity.GetPlaces())
-			{
-				// TODO: implement sorting for workplace and assign a workplace to the person
-				// to silence compiler warning
-				if (place->GetID() < 0) {}
-			}
+			// First choose a weighted workplace vector, then assigne uniform under the same size workplaces
+			std::random_device seed;
+			std::mt19937 generator(seed());
+			std::discrete_distribution<size_t> distribution({ 2649, 3080, 1908, 821, 1542 });
+			size_t distIndex = distribution(generator);
+			std::uniform_int_distribution<size_t> uniform(0, workplacesBySize.at(distIndex).size() - 1);
+			person->SetWorkplace(workplacesBySize.at(distIndex).at(uniform(generator)));
 		}
+		// Add the created person to the community
 		outCommunity.AddPerson(std::move(person));
 	}
 }
